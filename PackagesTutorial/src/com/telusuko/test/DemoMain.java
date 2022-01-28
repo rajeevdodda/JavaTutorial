b165e314928316dc471f0262eeced294c5a25e25
Rahul Dodda <rahul.dodda@infor.com>
2022-01-28 16:25:40 +0530

    java

--- PackagesTutorial/src/com/telusuko/test/DemoMain.java
+++ PackagesTutorial/src/com/telusuko/test/DemoMain.java
@@ -7,7 +7,7 @@ public class DemoMain {
     public static void main(String[] args) {
         Student student = new Student();
         student.rollNo = 9;
-        // student.gender = "Male";
+        // student.gender = "Male" ;
         student.marks = 20;
 
         Engineer e = new Engineer();
